9409dcf9de5cac438b81cd810b2a972c0c8042d1
liuyue <76907680+lymyname@users.noreply.github.com>
2025-01-03 14:38:32 +0800

    Update hero.tsx
    bug

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
-import homeSrc from "../public/a0.png";
+
 interface Iprops {
   imgUrl: StaticImageData;
   altTxt: string;
