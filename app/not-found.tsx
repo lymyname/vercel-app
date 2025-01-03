26f9d650dd2255a37efe8b1d7ab10802d6c06579
liuyue <76907680+lymyname@users.noreply.github.com>
2025-01-03 14:48:48 +0800

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
               <div className="text-5xl font-dark font-bold">404</div>
                 <p
                   className="text-2xl md:text-3xl font-light leading-normal"
-                >Sorry we couldn't find this page. </p>
+                >Sorry we couldn not find this page. </p>
               
               
               <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700" onClick={() => window.location.href = "/"}>back to homepage</button>
